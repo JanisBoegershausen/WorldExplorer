058e6b612a536c75fa03f097ed13e6b25df9014b
Janis Boegershausen (Pc) <janis.boegershausen@gmx.net>
2021-10-10 21:02:45 +0200

    Updated request URLs to match the new github page

--- js/Explorer.js
+++ js/Explorer.js
@@ -12,7 +12,7 @@ function LoadAllEntities() {
   entities = [];
   var entityJsonFiles = [];
 
-  var requestUrl = "https://api.github.com/repos/JanisBoegershausen/JanisBoegershausen.github.io/git/trees/master?recursive=1";
+  var requestUrl = "https://api.github.com/repos/JanisBoegershausen/WorldExplorer/git/trees/master?recursive=1";
   var fileListRequest = new XMLHttpRequest();
 
   fileListRequest.open("GET", requestUrl, true);
@@ -32,7 +32,7 @@ function LoadAllEntities() {
 
       entityJsonFiles.forEach((entityJsonFile) => {
         var entityJsonRequest = new XMLHttpRequest();
-        entityJsonRequest.open("GET", "https://janisboegershausen.github.io/" + entityJsonFile.path, true);
+        entityJsonRequest.open("GET", "https://janisboegershausen.github.io/WorldExplorer/" + entityJsonFile.path, true);
         entityJsonRequest.send(null);
 
         entityJsonRequest.onreadystatechange = function () {
