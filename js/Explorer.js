29ccf026f9b90e8244e4bfd353bbcf6134d8a872
Janis Boegershausen (Pc) <janis.boegershausen@gmx.net>
2021-10-10 21:45:29 +0200

    Changed the code to load all entites as objects on startup and added test entities

--- js/Explorer.js
+++ js/Explorer.js
@@ -2,31 +2,47 @@ var settings = {
   dataUrl: "https://janisboegershausen.github.io/WorldExplorer/data/",
 };
 
-var database;
+var entities = {};
 
 function setup() {
-  
+  LoadEntityDatabase();
 }
 
-// Load the database from the database.json file
-function LoadDatabase() {
-
-  // Get path to json file
-  var databaseUrl = dataUrl + "database.json";
+// Load the database from the database.json file and store the result in the entities dict
+function LoadEntityDatabase() {
+  // Get path to the database.json file, which is a dict of all ids and their relative file paths
+  var databaseUrl = settings.dataUrl + "database.json";
 
   // Create and send the request to read the file
   var fileListRequest = new XMLHttpRequest();
   fileListRequest.open("GET", databaseUrl, true);
   fileListRequest.send(null);
 
-  // Once the file was recieved, save the json data into the variable
+  // Once the file was recieved, do the following
   fileListRequest.onreadystatechange = function () {
     if (fileListRequest.readyState === 4 && fileListRequest.status === 200) {
-      database = JSON.parse(fileListRequest.responseText);
+      // Parse the filedatabase to an object
+      var fileDatabase = JSON.parse(fileListRequest.responseText);
+      // Get all keys and therefor the ids of all entities in the database
+      var entityIds = Object.keys(fileDatabase);
+      // Foreach entity id, save the id along with its entity in the entities variable
+      entityIds.forEach((entityId) => {
+        // Send a request to read the entities file
+        var entityRequest = new XMLHttpRequest();
+        entityRequest.open("GET", settings.dataUrl + fileDatabase[entityId], true);
+        entityRequest.send(null);
+
+        // Once the file was recieved, save the json data into the entites variable
+        entityRequest.onreadystatechange = function () {
+          if (entityRequest.readyState === 4 && entityRequest.status === 200) {
+            entities[entityId] = JSON.parse(entityRequest.responseText);
+          }
+        };
+      });
     }
-  }
+  };
 }
 
 function GetEntityById(id) {
-  return database[id];
+  return entities[id];
 }
