6f14936dbb050883f5860ce3b2e632d4d1f189bb
Janis Boegershausen (Pc) <janis.boegershausen@gmx.net>
2021-10-10 20:58:32 +0200

    Added system to automatically load all entity json files in a directory

---
+++ js/Explorer.js
@@ -0,0 +1,48 @@
+var settings = {
+  entityPath: "../Data/",
+};
+
+var entities = [];
+
+function setup() {
+  LoadAllEntities();
+}
+
+function LoadAllEntities() {
+  entities = [];
+  var entityJsonFiles = [];
+
+  var requestUrl = "https://api.github.com/repos/JanisBoegershausen/JanisBoegershausen.github.io/git/trees/master?recursive=1";
+  var fileListRequest = new XMLHttpRequest();
+
+  fileListRequest.open("GET", requestUrl, true);
+  fileListRequest.send(null);
+
+  var fileTree;
+
+  fileListRequest.onreadystatechange = function () {
+    if (fileListRequest.readyState === 4 && fileListRequest.status === 200) {
+      fileTree = JSON.parse(fileListRequest.responseText).tree;
+
+      fileTree.forEach((fileEntry) => {
+        if (fileEntry.type == "blob" && fileEntry.path.substring(0, 14) == "data/entities/") {
+          entityJsonFiles.push(fileEntry);
+        }
+      });
+
+      entityJsonFiles.forEach((entityJsonFile) => {
+        var entityJsonRequest = new XMLHttpRequest();
+        entityJsonRequest.open("GET", "https://janisboegershausen.github.io/" + entityJsonFile.path, true);
+        entityJsonRequest.send(null);
+
+        entityJsonRequest.onreadystatechange = function () {
+          if (fileListRequest.readyState === 4 && fileListRequest.status === 200) {
+            entities.push(JSON.parse(fileListRequest.responseText));
+          }
+        };
+      });
+
+      console.log(entityJsonFiles);
+    }
+  };
+}
