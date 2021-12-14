181c305c9754ee2421fe54a85dcd02cc5045b0a9
Janis Boegershausen (Pc) <janis.boegershausen@gmx.net>
2021-12-14 18:37:21 +0100

    Added drawing of Star Systems and automatic hiding of labels

--- js/Drawing.js
+++ js/Drawing.js
@@ -17,11 +17,60 @@ function DrawEntity(entity) {
         case "Planet":
             DrawPlanet(entity);
             break;
-
+        case "Star System":
+            DrawStarSystem(entity);
+            break;
         default:
             // ToDo: Draw Questionmark or something
             break;
     }
+}
+
+// Draw a given system
+function DrawStarSystem(system) {
+    // Calculate how big the system can be to still fit into the canvas
+    var maxDiameter = min(width, height);
+
+    // Setup
+    noStroke();
+
+    // Space background
+    background(5, 5, 10);
+
+    // Draw Sun
+    fill(255, 215, 0);
+    circle(width/2, height/2, 50, 50);
+
+    // Draw children
+    var i = 0;
+    system.children.forEach(childId => {
+        var child = GetEntityById(childId);
+        switch (child.type) {
+            case "Planet":
+                // Draw continent
+                noStroke();
+                fill(SeededRandom(child.seed) * 255, SeededRandom(child.seed + 1) * 255, SeededRandom(child.seed + 2) * 255, 255);
+                var positionRandom = SeededRandom(child.seed + 3) * 2 * PI;
+                var positionX = (width / 2) + maxDiameter * 0.4 * cos(positionRandom) * SeededRandom(child.seed + 4);
+                var positionY = (height / 2) + maxDiameter * 0.4 * sin(positionRandom) * SeededRandom(child.seed + 5);
+                var diameter = SeededRandom(child.seed + 6) * maxDiameter * 0.1 + 5;
+                ellipse(positionX, positionY, diameter, diameter)
+
+                if (abs(positionX - mouseX) < diameter / 2 && abs(positionY - mouseY) < diameter / 2) {
+                    // Draw label
+                    fill(255);
+                    stroke(0);
+                    strokeWeight(2);
+                    textStyle(BOLD);
+                    textAlign(CENTER, CENTER);
+                    text(child.displayName, positionX, positionY);
+                }
+                break;
+
+            default:
+                break;
+        }
+    });
 }
 
 // Draw a given planet
@@ -58,21 +107,23 @@ function DrawPlanet(planet) {
             case "Continent":
                 // Draw continent
                 noStroke();
-                fill(SeededRandom(child.seed) * 255, SeededRandom(child.seed+1) * 255, SeededRandom(child.seed+2) * 255, 255);
+                fill(SeededRandom(child.seed) * 255, SeededRandom(child.seed + 1) * 255, SeededRandom(child.seed + 2) * 255, 255);
                 var positionRandom = SeededRandom(child.seed + 3) * 2 * PI;
                 var positionX = (width / 2) + maxDiameter * 0.4 * cos(positionRandom) * SeededRandom(child.seed + 4);
                 var positionY = (height / 2) + maxDiameter * 0.4 * sin(positionRandom) * SeededRandom(child.seed + 5);
-                var w = SeededRandom(child.seed + 6) * maxDiameter * 0.1 + 5; 
-                var h = SeededRandom(child.seed + 7) * maxDiameter * 0.1 + 5; 
-                ellipse(positionX - w / 2, positionY - h / 2, w, h)
+                var w = SeededRandom(child.seed + 6) * maxDiameter * 0.2 + 5;
+                var h = SeededRandom(child.seed + 7) * maxDiameter * 0.2 + 5;
+                ellipse(positionX, positionY, w, h)
 
-                // Draw label
-                fill(255);
-                stroke(0);
-                strokeWeight(2);
-                textStyle(BOLD);
-                textAlign(CENTER, CENTER);
-                text(child.displayName, positionX - w / 2, positionY - h / 2);
+                if (abs(positionX - mouseX) < w / 2 && abs(positionY - mouseY) < h / 2) {
+                    // Draw label
+                    fill(255);
+                    stroke(0);
+                    strokeWeight(2);
+                    textStyle(BOLD);
+                    textAlign(CENTER, CENTER);
+                    text(child.displayName, positionX, positionY);
+                }
                 break;
 
             default:
